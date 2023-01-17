6173ccc0b5f9e1ef86eacb1938688b206e1f44fc
Vishwas <VishwasDubey150@gmail.com>
2023-01-17 17:51:11 +0530

    ...

---
+++ app/src/main/java/com/example/mywishlist/activities/place_details.kt
@@ -0,0 +1,12 @@
+package com.example.mywishlist.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.mywishlist.R
+
+class place_details : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_place_details)
+    }
+}
